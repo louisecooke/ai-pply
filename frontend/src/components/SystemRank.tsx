8318c3f6d6b48872c4c8da9a36fdbe647f56059a
LOUISE COOKE <louisecynthiacooke@gmail.com>
2022-07-14 15:46:11 +0200

    Alignment change

--- frontend/src/components/SystemRank.tsx
+++ frontend/src/components/SystemRank.tsx
@@ -1,33 +1,29 @@
-import { Stack, Card} from "@mui/material";
-
-import * as React from "react";
-import { Recommendation, Applicant } from "../types";
-
+import { Stack} from "@mui/material";
+import { Applicant } from "../types";
 import { Reorder } from "framer-motion/dist/framer-motion";
-
 import ApplicantLine from "./ApplicantLine";
 
 type Props = {
   applicants: Applicant[];
   setApplicants: Function;
+  writeExplanation: Function;
 };
 
-export default function SystemRank({applicants, setApplicants}: Props) {
+export default function SystemRank({applicants, setApplicants, writeExplanation}: Props) {
 
   function shortlist(applicant: Applicant) {
     let newList = [applicant, ...applicants.filter(a => a.id !== applicant.id)];
     setTimeout(setApplicants(newList), 1000);
-    //setApplicants();
   }
 
   const elements = () => {
     return applicants.map((a, i) => {
-    return (<ApplicantLine instance={a} index={i} key={a.id} shortlist={shortlist}/> );});
+    return (<ApplicantLine applicant={a} index={i} key={a.id} shortlist={shortlist} writeExplanation={writeExplanation}/> );});
   }
 
   return (
     <Reorder.Group axis="y" values={applicants} onReorder={setApplicants} as='div'>
-    <Stack alignItems='flex-end' direction='column' spacing={2} sx={{marginTop: 0, marginBottom: 10}}>
+    <Stack alignItems='flex-start' direction='column' spacing={2} sx={{marginTop: 0, marginBottom: 10}}>
       {elements()}
     </Stack>
     </Reorder.Group>
